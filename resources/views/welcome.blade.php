278f970cba7f8294f86fbf699f6b297ace57405a
omar901 <omarsharif.zahra@gmail.com>
2021-06-17 05:27:08 -0400

    stage 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,12 @@
             </div> 
             <div>
                <img class="h-400 rounded-2xl shadow-lg" src="img/playa_caracas_vqs.jpg" alt="Playa Caracas Vieques PR">
-            </div>    
+            </div>   
+            <div>
+             <h1 class="text-center font-bold font-2xl">
+                787 718-8869
+             </h1>
+            </div> 
         </div>   
     </body>
 </html>
