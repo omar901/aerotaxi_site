d771b9ad0e684a01ac2596341290b1d2649f6c0e
omar901 <omarsharif.zahra@gmail.com>
2021-06-17 05:33:28 -0400

    stage 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,11 +20,11 @@
                <img class="h-400 rounded-2xl shadow-lg mx-auto" src="img/playa_caracas_vqs.jpg" alt="Playa Caracas Vieques PR">
             </div>   
             <div>
-             <h1 class="text-center font-bold font-4xl text-gray-900 py-6">
+             <h1 class="text-center font-bold font-4xl text-green-600 py-6">
                 787 718-8869
                 
              </h1>
-             <h1 class="text-center font-bold font-4xl text-gray-900 py-6">
+             <h1 class="text-center font-bold font-4xl text-green-600 py-6">
                 reservations@taxiaereopr.travel
                 
              </h1>
