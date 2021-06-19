6491f356c5b6359db2a90af7c20e4a1786e2e628
omar901 <omarsharif.zahra@gmail.com>
2021-06-19 04:58:00 -0400

    added shadow to logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    textColor: theme => theme('colors'),
+    textColor: {
+      'babyBlue': '2D7DB0',
+    },
     extend: {},
   },
   variants: {
