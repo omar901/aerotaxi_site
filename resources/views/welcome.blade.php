a73bf3f249daa3de39c49ad13b951ec0fa8b8672
omar901 <omarsharif.zahra@gmail.com>
2021-06-19 05:43:56 -0400

    added shadow to logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,11 +20,11 @@
                <img class="h-400 rounded-2xl shadow-lg mx-auto" src="img/playa_caracas_vqs.jpg" alt="Playa Caracas Vieques PR">
             </div>   
             <div>
-             <h1 class="text-center font-bold font-4xl text-blue-light py-6">
+             <h1 class="text-center font-bold font-4xl text-blue-300 py-6">
                 787 718-8869
                 
              </h1>
-             <h1 class="text-center font-bold font-4xl text-blue py-6">
+             <h1 class="text-center font-bold font-4xl text-blue-300 py-6">
                 reservations@taxiaereopr.travel
                 
              </h1>
