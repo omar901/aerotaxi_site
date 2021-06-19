a20343adec4fbb41e0984b33fadba14ab612786e
omar901 <omarsharif.zahra@gmail.com>
2021-06-19 04:49:50 -0400

    added shadow to logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
     <body class="bg-gray-100">
         <div class="flex-1 max-w-4xl p-10 mx-auto">
             <div class="mx-auto px-8">
-                <img class="h-100 mx-auto" src="img/taxiaereologo_shadow_trans.png" alt="logo">
+                <img class="h-100 mx-auto py-2" src="img/taxiaereologo_shadow_trans.png" alt="logo">
             </div> 
             <div>
                <img class="h-400 rounded-2xl shadow-lg mx-auto" src="img/playa_caracas_vqs.jpg" alt="Playa Caracas Vieques PR">
