d4fe507bd3222f752a67a700f049c1159736fe2e
omar901 <omarsharif.zahra@gmail.com>
2021-06-20 19:11:47 -0400

    foto display thumbnails

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,17 @@
     <div class="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
       <img src="img/flamenco_tank.jpg" alt="" class="object-cover pointer-events-none group-hover:opacity-75">
       <button type="button" class="absolute inset-0 focus:outline-none">
-        <span class="sr-only">View details for IMG_4985.HEIC</span>
+        <span class="sr-only">View details for Flamenco beach, Culebra PR</span>
+      </button>
+    </div>
+    <p class="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">Flamenco Beach</p>
+    <p class="block text-sm font-medium text-gray-500 pointer-events-none">Culebra, PR</p>
+  </li>
+  <li class="relative">
+    <div class="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
+      <img src="img/flamenco_tank.jpg" alt="" class="object-cover pointer-events-none group-hover:opacity-75">
+      <button type="button" class="absolute inset-0 focus:outline-none">
+        <span class="sr-only">View details for Flamenco beach, Culebra PR</span>
       </button>
     </div>
     <p class="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">Flamenco Beach</p>
