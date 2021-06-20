77c492e5b588ff664e97c97e7a2ccc009ef45129
omar901 <omarsharif.zahra@gmail.com>
2021-06-20 19:55:37 -0400

    foto display thumbnails

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,6 +51,16 @@
     <p class="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">Caracas Beach</p>
     <p class="block text-sm font-medium text-gray-500 pointer-events-none">Vieques, PR</p>
   </li>
+  <li class="relative">
+    <div class="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
+      <img src="img/sunset_ceiba.jpg" alt="" class="object-cover pointer-events-none group-hover:opacity-75">
+      <button type="button" class="absolute inset-0 focus:outline-none">
+        <span class="sr-only">View details for Flamenco beach, Culebra PR</span>
+      </button>
+    </div>
+    <p class="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">Flamenco Beach</p>
+    <p class="block text-sm font-medium text-gray-500 pointer-events-none">Culebra, PR</p>
+  </li>
 
   <!-- More files... -->
 </ul>
