12a8a91ba057fdf681947b20eb834fb54cb7ad05
omar901 <omarsharif.zahra@gmail.com>
2021-06-20 20:50:41 -0400

    added livewire

--- app/Http/Livewire/PictureExpand.php
+++ app/Http/Livewire/PictureExpand.php
@@ -7,7 +7,13 @@ use Livewire\Component;
 class PictureExpand extends Component
 {
     public $name = 'Visitor';
-    
+    protected $listeners = ['refreshpicture' => 'expandme'];
+
+    public function expandme()
+    {
+        $this->emit('expandme');
+    }
+
     public function render()
     {
         return view('livewire.picture-expand', [
--- resources/views/livewire/picture-expand.blade.php
+++ resources/views/livewire/picture-expand.blade.php
@@ -2,4 +2,17 @@
     <span>Enter your name</span>
     <input wire:model="name" type="text">
    Hello {{strtoupper($name)}}
+
+   <ul role="list" class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
+  <li class="relative">
+    <div class="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
+      <img wire:click="refreshpicture" src="img/flamenco_tank.jpg" alt="" class="object-cover pointer-events-none group-hover:opacity-75">
+      <button type="button" class="absolute inset-0 focus:outline-none">
+        <span class="sr-only">View details for Flamenco beach, Culebra PR</span>
+      </button>
+    </div>
+    <p class="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">Flamenco Beach</p>
+    <p class="block text-sm font-medium text-gray-500 pointer-events-none">Culebra, PR</p>
+  </li>
+   </ul>
 </div>
