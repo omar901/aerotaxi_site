209a7082acc1d50aaca27a6d994d07ef0f8d86f2
omar901 <omarsharif.zahra@gmail.com>
2021-06-21 05:16:14 -0400

    added livewire

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,40 +21,7 @@
             <div>
 
 
-<ul role="list" class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-  <li class="relative">
-    <div class="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
-      <img wire:click="refreshpicture" src="img/flamenco_tank.jpg" alt="" class="object-cover pointer-events-none group-hover:opacity-75">
-      <button type="button" class="absolute inset-0 focus:outline-none">
-        <span class="sr-only">View details for Flamenco beach, Culebra PR</span>
-      </button>
-    </div>
-    <p class="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">Flamenco Beach</p>
-    <p class="block text-sm font-medium text-gray-500 pointer-events-none">Culebra, PR</p>
-  </li>
-  <li class="relative">
-    <div class="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
-      <img src="img/playa_caracas_vqs.jpg" alt="" class="object-cover pointer-events-none group-hover:opacity-75">
-      <button type="button" class="absolute inset-0 focus:outline-none">
-        <span class="sr-only">View details for Playa Caracas, Vieques PR</span>
-      </button>
-    </div>
-    <p class="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">Caracas Beach</p>
-    <p class="block text-sm font-medium text-gray-500 pointer-events-none">Vieques, PR</p>
-  </li>
-  <li class="relative">
-    <div class="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
-      <img src="img/sunset_ceiba.jpg" alt="" class="object-cover pointer-events-none group-hover:opacity-75">
-      <button type="button" class="absolute inset-0 focus:outline-none">
-        <span class="sr-only">Sunset in Ceiba PR</span>
-      </button>
-    </div>
-    <p class="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">Ceiba Sunset</p>
-    <p class="block text-sm font-medium text-gray-500 pointer-events-none">Ceiba, PR</p>
-  </li>
 
-  <!-- More files... -->
-</ul>
 
             </div> 
         </div> 
