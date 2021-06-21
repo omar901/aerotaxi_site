3d45a0a1ac03189b720e765e2c1ab956bc1d2622
omar901 <omarsharif.zahra@gmail.com>
2021-06-20 21:12:40 -0400

    added livewire

--- resources/views/livewire/picture-expand.blade.php
+++ resources/views/livewire/picture-expand.blade.php
@@ -6,7 +6,7 @@
    <ul role="list" class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
   <li class="relative">
     <div class="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
-      <img  src="img/"{{$picture}} alt="" class="object-cover pointer-events-none group-hover:opacity-75">
+      <img  src="img/{{$picture}}" alt="" class="object-cover pointer-events-none group-hover:opacity-75">
       <button wire:click="refreshpicture" type="button" class="absolute inset-0 focus:outline-none">
         <span class="sr-only">View details for Flamenco beach, Culebra PR</span>
       </button>
