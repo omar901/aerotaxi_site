f841f966a93f21b69219fe507b7b56f9fa69317c
omar901 <omarsharif.zahra@gmail.com>
2021-06-22 04:51:47 -0400

    added livewire

--- resources/views/livewire/picture-expand.blade.php
+++ resources/views/livewire/picture-expand.blade.php
@@ -4,7 +4,7 @@
             </div>
             <div class="mx-auto">
             <ul role="list" class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8 mt-3">
-  <li class="relative mx-auto">
+  <li class="relative">
     <div class="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden ">
       <img src="img/flamenco_tank.jpg" alt="" class="object-cover pointer-events-none group-hover:opacity-75">
       <button type="button" class="absolute inset-0 focus:outline-none">
