76a3454ed6c9d7b50f25561ffab6954355b6debc
omar901 <omarsharif.zahra@gmail.com>
2021-06-23 19:13:37 -0400

    added livewire

--- resources/views/livewire/tank.blade.php
+++ resources/views/livewire/tank.blade.php
@@ -1,10 +1,7 @@
-<div>
-
-            
-               
-            <div>
-                <img class="h-400 rounded-2xl shadow-lg mx-auto" src="img/playa_caracas_vqs.jpg" alt="Playa Caracas Vieques PR">
-            </div>
+<div>        
+    <div class="mt-5">
+        <img class="h-400 rounded-2xl shadow-lg mx-auto" src="img/playa_caracas_vqs.jpg" alt="Playa Caracas Vieques PR">
+    </div>
 
 </div>
         
