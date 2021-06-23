51e10f6c57c9c29b2d849bf5ad695c1d11dee527
omar901 <omarsharif.zahra@gmail.com>
2021-06-23 06:07:37 -0400

    added livewire

--- resources/views/livewire/tank.blade.php
+++ resources/views/livewire/tank.blade.php
@@ -4,10 +4,8 @@
                
             <div>
                 <img class="h-400 rounded-2xl shadow-lg mx-auto" src="img/flamenco_tank.jpg" alt="Playa Caracas Vieques PR">
-                </div>
-                <div class="mx-auto">
-                <ul role="list" class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8 mt-3">
-            </div> 
+            </div>
+
 </div>
         
        
