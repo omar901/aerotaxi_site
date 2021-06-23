2b7e153a18bed4d0c928cb966020da5270f78d53
omar901 <omarsharif.zahra@gmail.com>
2021-06-23 06:03:57 -0400

    added livewire

--- resources/views/livewire/tank.blade.php
+++ resources/views/livewire/tank.blade.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        @livewireStyles
-    </head>
-    
-    <body class="bg-gray-100">
-        <div class="flex-1 max-w-4xl p-10 mx-auto">
+<div>
+<div class="flex-1 max-w-4xl p-10 mx-auto">
             
                
             <div>
@@ -22,9 +8,8 @@
                 <div class="mx-auto">
                 <ul role="list" class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8 mt-3">
             </div> 
+</div>
+        
        
 
-        @livewireScripts  
-        <link href="{{asset('js/app.js') }}">
-    </body>
-</html>
+       
