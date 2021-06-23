1295f571d5944d9289d5a66dde84053868cfc2bc
omar901 <omarsharif.zahra@gmail.com>
2021-06-23 05:52:49 -0400

    added livewire

--- app/Http/Livewire/PictureExpand.php
+++ app/Http/Livewire/PictureExpand.php
@@ -2,23 +2,12 @@
 
 
 namespace App\Http\Livewire;
-header("Content-type: image/jpg");
+
 use Livewire\Component;
 
 class PictureExpand extends Component
 {
-    public $name = 'Visitor';
-    public $picture = '/flamenco_tank.jpg';
-    protected $listeners = ['expandme' => 'expandme'];
-
- function expandme()
- {
-     return view('/tank');
- }
- public function mount()
- {
-     $this->picture = '/flamenco_tank.jpg';
- }
+    
     public function render()
     {
         return view('livewire.picture-expand');
